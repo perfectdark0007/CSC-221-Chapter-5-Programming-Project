d08db5957ec957c325581edb8b1b9025bd60a1fe
perfectdark0007 <woodardwjun@gmail.com>
2025-04-12 20:23:54 -0400

    p3

--- CSC-221 Chapter 5 Programming Project/CSC-221 Chapter 5 Programming Project.cpp
+++ CSC-221 Chapter 5 Programming Project/CSC-221 Chapter 5 Programming Project.cpp
@@ -1,10 +1,51 @@
 // CSC-221 Chapter 5 Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+// 
 
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello Professor Schaffner, hopefully this works!\n";
+int main() {
+    string townName, fileName;
+
+    // Get town name from user
+    cout << "Enter the name of the city/town: ";
+    getline(cin, townName);
+
+    // Get file name from user
+    cout << "Enter the name of the data file: ";
+    getline(cin, fileName);
+
+    // Add .txt if not included in the filename
+    if (fileName.find(".txt") == string::npos) {
+        fileName += ".txt";
+    }
+
+    // Open file
+    ifstream inputFile(fileName);
+    if (!inputFile) {
+        cerr << "Error: Could not open the file \"" << fileName << "\"." << endl;
+        return 1;
+    }
+
+    // Print title
+    cout << endl;
+    cout << townName << " Population Growth" << endl;
+    cout << "(each * represents 1,000 people)" << endl;
+
+    int year, population;
+    while (inputFile >> year >> population) {
+        cout << year << " ";
+        int stars = max(1, population / 1000); // safer for small population
+        for (int i = 0; i < stars; ++i) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    inputFile.close();
+    return 0;
 }
 
